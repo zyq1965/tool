415fe730f3b77f12c34468d39a6b779f6cddcb05
zyq1965 <134155164+zyq1965@users.noreply.github.com>
2023-05-21 00:19:12 +0800

    5-21 0:19

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+<div>
+    <h1>OneDrive直链转换</h1>
+    <el-input aria-placeholder="请输入OneDrive分享链接"></el-input>
+    <el-button type="primary">转换</el-button>
+</div>
 </template>
