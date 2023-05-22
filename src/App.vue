d60fe3eafb194c590a85c8fe97d2524a4fe05096
admin <admin@zhaoyuqi.site>
2023-05-22 04:16:05 +0000

    del video

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,4 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 <template>
 <a href="https://v1t9dq92cl4.feishu.cn/file/IUZ4bjVyMoh0oyxcAuxcRFEwnMc">答辩PPT</a>
-<video src="https://iyyqq-my.sharepoint.com/personal/iyyqq_iyyqq_onmicrosoft_com/_layouts/52/download.aspx?share=EZZlV70kwvVNnxSwcomGJhcBvB9OQHNzNzM0fIGeYy4uRw"></video>
 </template>
