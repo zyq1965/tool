98bce86cffb2a9fa28f66a7985d834f464b8742b
admin <admin@zhaoyuqi.site>
2023-05-22 04:29:42 +0000

    add file

--- src/views/pages/File.vue
+++ src/views/pages/File.vue
@@ -4,6 +4,9 @@
 <template>
     <div>
         <h1>文件</h1>
+        
+    <a href="https://v1t9dq92cl4.feishu.cn/file/IUZ4bjVyMoh0oyxcAuxcRFEwnMc">答辩PPT</a>
+    
     </div>
 
 </template>
--- src/views/pages/OneDrive.vue
+++ src/views/pages/OneDrive.vue
@@ -4,5 +4,7 @@
 <template>
   <div>
     <h1>OneDrive直链转换</h1>
+    <video src="https://iyyqq-my.sharepoint.com/personal/iyyqq_iyyqq_onmicrosoft_com/_layouts/52/download.aspx?share=EZZlV70kwvVNnxSwcomGJhcBvB9OQHNzNzM0fIGeYy4uRw" controls="controls" width="100%" height="100%"></video>
+
   </div>
 </template>
